f0dc7884b9ebc4fd2bf18ca8f48e15240d6f5ea5
Granome <101628967+Granome@users.noreply.github.com>
2023-07-09 10:54:57 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,12 +11,12 @@ from telethon import utils
 from telethon import TelegramClient
 from telethon.tl import functions, types
 
-client = TelegramClient('Parser', '16559463', '757c0689271ce655b48df86483fdb51b')
+client = TelegramClient('Parser', '1111111111', '1111111111111111111111111')
 client.start()
 
 
 async def main():
-    dubbers = ["https://t.me/gm_video","https://t.me/inarivideo", "https://t.me/melvoice", "https://t.me/fanvoxua_video", "https://t.me/amanogawa_release", "https://t.me/dzuski_base", "https://t.me/clan_kaizoku"]
+    dubbers = ["https://t.me/someFollowedChannels"]
     quantity = 0
 
     with open("lastDate.txt", "r") as fp:
@@ -26,14 +26,14 @@ async def main():
         async for message in client.iter_messages(i, reverse = True, offset_date = lastDate):
             if message.video != None and message.text != None:
 
-                await client.forward_messages("https://t.me/AnimeUkrDubBot", messages=message)
+                await client.forward_messages("https://t.me/MyBot", messages=message)
                 quantity += 1
     deltaTime = datetime.datetime.utcnow() - lastDate
 
     with open("lastDate.txt", "w") as fp:
         fp.write(str(datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S")))
 
-    await client.send_message("https://t.me/AnimeUkrDubBot", "/getFile")
+    await client.send_message("https://t.me/MyBot", "/getFile")
     print(f"Перекинуто {quantity} відео за {deltaTime} часу")
     i = input("Enter Щоб закінчити")
 
